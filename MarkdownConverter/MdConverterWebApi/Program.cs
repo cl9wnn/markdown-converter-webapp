c3bfa21f56c561c05d233578d6fe971788729822
cl9wnn <egor_udir@mail.ru>
2024-12-03 16:45:29 +0300

    Add Login/Register Pages

--- MarkdownConverter/MdConverterWebApi/Program.cs
+++ MarkdownConverter/MdConverterWebApi/Program.cs
@@ -4,9 +4,22 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 
-app.MapGet("/md", async context =>
+app.MapGet("/", async context =>
 {
     context.Response.ContentType = "text/html";
     await context.Response.SendFileAsync("wwwroot/MainPage/index.html");
 });
+
+app.MapGet("/login", async context =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.SendFileAsync("wwwroot/LoginPage/index.html");
+});
+
+app.MapGet("/register", async context =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.SendFileAsync("wwwroot/RegisterPage/index.html");
+});
+
 app.Run();
